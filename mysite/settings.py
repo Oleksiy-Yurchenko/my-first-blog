7fc824b2d9c6ad855c9f16456fb050d94c3be449
Oleksiy Yurchenko <oleksiy.yurchenko@gmail.com>
2020-08-04 13:17:38 +0300

    Changed DEDBUG to False in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ynkg#jmwk0+d%99v6q43yq6iu73^9^xq#41_ipz9hu@jmo54k0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['oleksiyyurchenko.pythonanywhere.com', '127.0.0.1']
 
